cf8e23ff2df80f3f0b5f514538b3b9bf1824a9d6
Eric Caleb <calebme@mail.com>
2016-10-24 09:57:51 +0100

    Imported react router

--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -5,4 +5,20 @@ import { render } from 'react-dom';
 // import css
 import css from './styles/style.styl';
 
-render(<p>hello</p>,document.getElementById('root'));
+// import components
+import Main from './components/Main';
+import Single from './components/Single';
+import PhotoGrid from './components/PhotoGrid';
+
+//import react router deps
+import { Router , Route , IndexRoute, browserHistory } from 'react-router';
+
+const router = (
+  <Router history={browserHistory}>
+    <Route path="/" component={Main}>
+      
+    </Route>
+  </Router>
+)
+
+render(<Main/>, document.getElementById('root'));
