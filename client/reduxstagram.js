4e8f42bf94c8bd113a14f8ef5315b1b824cb0b9c
Eric Caleb <calebme@mail.com>
2016-10-24 10:59:41 +0100

    Added routers for components

--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -16,9 +16,10 @@ import { Router , Route , IndexRoute, browserHistory } from 'react-router';
 const router = (
   <Router history={browserHistory}>
     <Route path="/" component={Main}>
-      
+      <IndexRoute component={PhotoGrid}></IndexRoute>
+        <Route path="view/:postId" component={Single}></Route>
     </Route>
   </Router>
 )
 
-render(<Main/>, document.getElementById('root'));
+render(router, document.getElementById('root'));
