185b672c75a6fe6aa30dd8f50a08275dff7e6626
Eric Caleb <calebme@mail.com>
2016-10-25 10:56:37 +0100

    Modified Remove comment action

--- client/actions/actionCreators.js
+++ client/actions/actionCreators.js
@@ -21,7 +21,9 @@ export function addComment(postId, author, comment) {
 //remove comment
 
 export function removeComment(postId, i) {
-  type: "REMOVE_COMMENT",
-  i,
-  postId
+  return{
+    type: "REMOVE_COMMENT",
+    i,
+    postId
+  }
 }
