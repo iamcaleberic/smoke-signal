f0da46681bed90add32049ef2b12a9fa351d4771
Eric Caleb <calebme@mail.com>
2016-10-25 08:42:09 +0100

    Modified single component

--- client/components/Single.js
+++ client/components/Single.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import Photo from './Photo';
+// import comments
 
 const Single =  React.createClass({
   render(){
+    // index of post
+    const i = this.props.posts.findIndex((post) => post.code ===
+    this.props.params.postId);
+    const post = this.props.posts[i];
+
     return(
       <div className="single-photo">
-        Single nigga
+          <Photo i={i} post={post} {...this.props}/>
       </div>
      )
   }
