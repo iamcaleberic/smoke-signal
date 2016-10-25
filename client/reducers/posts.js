4a8ee3bafa4112186a11041f8a252a5639d79f27
Eric Caleb <calebme@mail.com>
2016-10-25 08:31:37 +0100

    Updated posts reducer

--- client/reducers/posts.js
+++ client/reducers/posts.js
@@ -1,8 +1,18 @@
 // for reducer send action and copy of current state
 function posts(state= [] , action) {
-    console.log("I am changing");
-    console.log(state, action);
-    return state;
+    switch(action.type){
+      case 'INCREMENT_LIKES':
+        console.log("incrementing likes");
+        const i = action.index;
+        return [
+          ...state.slice(0, i), //before post being updated
+          {...state[i], likes: state[i].likes + 1},
+          ...state.slice(i + 1), //after post being updated
+        ]
+      default:
+        return state;
+
+    }
 }
 
 export default posts;
